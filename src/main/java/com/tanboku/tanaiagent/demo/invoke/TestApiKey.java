d028d9e62721b8a3467a74e5405c404a344c9c5b
tanboku <3279180882@qq.com>
2026-06-20 16:22:54 +0800

    测试aidemo

---
+++ src/main/java/com/tanboku/tanaiagent/demo/invoke/TestApiKey.java
@@ -0,0 +1,5 @@
+package com.tanboku.tanaiagent.demo.invoke;
+
+public interface TestApiKey {
+    String API_KEY = "sk-f6cc6837a14d44728a2f0e49df9e6931";
+}
---
+++ src/test/java/com/tanboku/tanaiagent/TanAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanboku.tanaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
